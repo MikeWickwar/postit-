f5ba8081820b078f76436491bbd64b83022aaa87
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-18 16:00:55 -0700

    working form with an array of objects catching the forms

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ var app = angular.module('mike', []);
 
 app.controller('MyController', function ($scope) {
 
-  $scope.contractors = [];
+  $scope.forms = [];
   $scope.showMe = false
   $scope.toggleShow = function () {
     $scope.showMe = !$scope.showMe
+    console.log($scope.showMe);
   }
 
-  $scope.getContractor = function () {
-    var contractor = {}
-    contractor.name = $scope.name;
-    contractor.rate = $scope.rate;
-    $scope.contractors.push(contractor);
-    if ($scope.contractors.length > 0) {
-      $scope.buttontitle = "Add another contractor"
-    }
-    if ($scope.contractors.length > 2) {
-      $scope.buttontitle = "Really!?"
-    }
-    console.log($scope.contractors.length);
-    console.log('I have recieved '+$scope.name+' and '+$scope.rate);
-    console.log($scope.contractors);
+  $scope.submitForm= function () {
+    var form = {}
+    form.title = $scope.title;
+    form.author = $scope.author;
+    form.img_url = $scope.img_url;
+    form.description = $scope.description;
+    $scope.forms.push(form);
+
+    console.log($scope.forms);
   }
 })
