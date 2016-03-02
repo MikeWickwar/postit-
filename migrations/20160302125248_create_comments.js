268b6669ad4564fd81b21dae372d9ccdb752c60e
Mike Wickwar <mike.wickwar@gmail.com>
2016-03-02 13:35:41 -0700

    fixed seeds getting ready to run

--- migrations/20160302125248_create_comments.js
+++ migrations/20160302125248_create_comments.js
@@ -1,8 +1,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('readit_comments', function(table) {
-    table.increments();
+    table.increments('comment_id');
     table.integer('post_id');
-    table.text('comment')
+    table.text('comment');
   })
 };
 exports.down = function(knex, Promise) {
